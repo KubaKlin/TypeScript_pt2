3329660d2367260bf165d35f5bf2bb783fd18d96
Kuba Klin <poczta@kubaklin.pl>
2025-09-17 21:54:15 +0200

    repo init

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import { checker } from 'vite-plugin-checker';
+import react from '@vitejs/plugin-react-swc';
+
+export default defineConfig({
+  plugins: [
+    react(),
+    checker({
+      typescript: {
+        buildMode: true,
+      },
+    }),
+  ],
+});
